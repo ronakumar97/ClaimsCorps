325619d85b2bf48cf81c431df803465cd4a8d7e1
ronakumar97 <lifecoderonak2016@gmail.com>
2022-04-28 11:20:04 -0400

    Added updated pickle files

--- model.py
+++ model.py
@@ -3,14 +3,14 @@ import pickle
 def run_model(model_choice, costs_choice, col_values):
     if(model_choice == 'Repair Cost'):
         if(costs_choice == 'With Cost'):
-            with open('modelwithamounts_rc.pkl', 'rb') as f:
+            with open('model_directory/modelwithamounts_rc.pkl', 'rb') as f:
                 model = pickle.load(f)
                 X = []
                 X.append(list(col_values.values()))
                 prediction = model.predict(X)[0]
                 return prediction
         else:
-            with open('modelwithoutamounts_rc.pkl', 'rb') as f:
+            with open('model_directory/modelwithoutamounts_rc.pkl', 'rb') as f:
                 model = pickle.load(f)
                 X = []
                 X.append(list(col_values.values()))
@@ -18,14 +18,14 @@ def run_model(model_choice, costs_choice, col_values):
                 return prediction
     else:
         if (costs_choice == 'With Cost'):
-            with open('modelwithamounts_pmc.pkl', 'rb') as f:
+            with open('model_directory/modelwithamounts_pmc.pkl', 'rb') as f:
                 model = pickle.load(f)
                 X = []
                 X.append(list(col_values.values()))
                 prediction = model.predict(X)[0]
                 return prediction
         else:
-            with open('modelwithoutamounts_pmc.pkl', 'rb') as f:
+            with open('model_directory/modelwithoutamounts_pmc.pkl', 'rb') as f:
                 model = pickle.load(f)
                 X = []
                 X.append(list(col_values.values()))
